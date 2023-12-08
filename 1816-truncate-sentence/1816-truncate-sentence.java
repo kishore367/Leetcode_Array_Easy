55aa03ca4e72676eaf7109294600636de875ca2d
kishore E <110755404+kishore367@users.noreply.github.com>
2023-12-08 08:32:43 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (34.01%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+
+        int count=0;
+        int i;
+        for(i=0;i<s.length();i++){
+            if(s.charAt(i)==' ')
+            count++;
+
+            if(count==k)
+
+            break;
+        }
+        if(i==s.length()){
+            return s;
+        }
+        return s.substring(0,i);
+    }
+       
+
+}
